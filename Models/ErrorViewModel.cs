a58eb91676eeb74b7dd9ce1d2c8baee1c57028da
sapani <sapani@USBLRSAPANI1>
2022-10-13 12:41:25 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WorkspaceBooking.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
